46f4d1181121b3284e9980429c9f26b103b18ed8
Rafif-fw <113953893+Rafif-fw@users.noreply.github.com>
2022-09-20 12:49:10 +0700

    Add files via upload

---
+++ Pratikum 1 064002200001 Rafif Fernanda.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 18 13:14:25 2022
+
+@author: Rafif Fernanda
+"""
+
+s1=input("siapa nama kamu?")
+s2=input("darimana asal kamu rafif?")
+s3=input("sebelumnya bersekolah dimana?")
+
+
+print("hi"+ s1, "wah dari"+ s2, "pasti menyenangkan bersekolah di"+ s3)
+
---
+++ pratikum 2 06400220001 Rafif Fernanda.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 20 08:32:41 2022
+
+@author: Rafif Fernanda
+"""
+s1=int(input("masukan pajang ruangan="))
+s2=int(input("masukan lebar ruangan="))
+s3=input("masukan satuan(meter/inci)=")
+
+
+luas=s1*s2
+
+
+print("luas ruangan dengan panjang",  s1, "dan lebar", s2, "adalah",  luas, s3)
